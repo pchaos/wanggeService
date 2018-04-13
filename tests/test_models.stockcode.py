32f04e0b97aefda99b4ba8ccb59eb387e23bf04d
pchaosgit <drifthua@gmail.com>
2018-04-13 12:23:46 +0800

    init

---
+++ tests/test_models.stockcode.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+
+@File    : test_models.stockcode.py
+
+Description :
+
+@Author :       pchaos
+
+date：          18-4-12
+-------------------------------------------------
+Change Activity:
+               18-4-12:
+@Contact : p19992003#gmail.com                   
+-------------------------------------------------
+"""
+
+from django.test import TestCase as DTEST
+# from unittest import TestCase
+
+__author__ = 'pchaos'
+
+from wangges.models import Stockcode as SC, ZXG, MARKET_CHOICES, YES_NO
+
+class TestStockcode(DTEST):
+    def setup(self):
+        print("======in setUp")
+
+    def tearDown(self):
+        print("======in tearDown")
+
+    def test_Stockcode(self):
+        sc = SC(code='tt0001', name='Test0001', market=MARKET_CHOICES[0])
+        sc.save()
+        i= sc.objects.all().count()
+        self.assertTrue(i > 0, 'Stockcode count:{}'.format(i))
+
+if __name__ == '__main__':
+    DTEST.run()

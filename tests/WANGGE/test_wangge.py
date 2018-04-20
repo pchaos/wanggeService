c27a668b175c6a34f439a9eccaabeda08d6b5510
pchaosgit <drifthua@gmail.com>
2018-04-20 17:20:30 +0800

    修复浮点数bug

--- tests/WANGGE/test_wangge.py
+++ tests/WANGGE/test_wangge.py
@@ -31,21 +31,21 @@ class TestWangge(TestCase):
     def test_doCaculateSimpleWangge(self):
         wangge = self.wg()
         print(wangge)
-        self.assertTrue(wangge[-1][1] == self.low, "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
         self.high = 1.323
         self.low = 0.414
         wangge = self.wg(self.high, self.low, self.n)
         print(wangge)
-        self.assertTrue(wangge[-1][1] == self.low, "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
         self.n = 30
         wangge = self.wg(self.high, self.low, self.n)
         print(wangge)
-        self.assertTrue(wangge[-1][1] == self.low, "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
         self.n = 40
         wangge = self.wg(self.high, self.low, self.n)
         print(wangge)
-        self.assertTrue(wangge[-1][1] == self.low, "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
-        self.assertTrue(wangge[0][1] == self.high, "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[0][1], self.high), "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
 
     def test_doCaculateROExWangge(self):
         """
@@ -59,9 +59,10 @@ class TestWangge(TestCase):
         self.wg = ROEWangge(self.high, self.low, self.n)
         wangge = self.wg()
         print(wangge)
-        self.assertTrue(wangge[-1][1] == self.low, "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
-        self.assertTrue(wangge[0][1] == self.high, "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
-        self.assertTrue(wangge[self.n // 2][1] < (self.high+self.low)/2, "计算错误，计算结果和实际网格中间值: {0} < {1}".format(wangge[self.n // 2][1], (self.high+self.low)/2))
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[0][1], self.high), "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
+        self.assertTrue(wangge[self.n // 2][1] < (self.high + self.low) / 2,
+                        "计算错误，计算结果和实际网格中间值: {0} < {1}".format(wangge[self.n // 2][1], (self.high + self.low) / 2))
 
         self.high = 200
         self.low = 144
@@ -70,8 +71,8 @@ class TestWangge(TestCase):
         self.wg = ROEWangge(self.high, self.low, self.n)
         wangge = self.wg()
         print(wangge)
-        self.assertTrue(wangge[-1][1] == self.low, "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
-        self.assertTrue(wangge[0][1] == self.high, "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[0][1], self.high), "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
 
         self.high = 1.323
         self.low = 0.414
@@ -80,5 +81,19 @@ class TestWangge(TestCase):
         self.wg = ROEWangge(self.high, self.low, self.n)
         wangge = self.wg()
         print(wangge)
-        self.assertTrue(wangge[-1][1] == self.low, "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
-        self.assertTrue(wangge[0][1] == self.high, "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[0][1], self.high), "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
+
+        # 603180 2018 04 20
+        self.high = 179.9
+        self.low = np.round(self.high * 0.8, 2)
+        self.n = 9  # 网格为8格
+        # self.wg = wangGebase(self.high, self.low, self.n)
+        self.wg = ROEWangge(self.high, self.low, self.n)
+        wangge = self.wg()
+        print(wangge)
+        self.assertTrue(self.isequal(wangge[-1][1], self.low), "计算错误，计算结果和实际网格最小值不相等: {0} != {1}".format(wangge[-1][1], self.low))
+        self.assertTrue(self.isequal(wangge[0][1], self.high), "计算错误，计算结果和实际网格最大值不相等: {0} != {1}".format(wangge[0][1], self.high))
+
+    def isequal(self, value1, value2):
+        return np.abs(value1 - value2) < 0.0001

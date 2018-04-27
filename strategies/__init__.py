646e14352730a2dfce20fb09385d5dfb9079d8d2
pchaosgit <drifthua@gmail.com>
2018-04-27 16:15:46 +0800

    斐波那契数列

---
+++ strategies/__init__.py
@@ -0,0 +1,17 @@
+# 斐波那契数列
+def Fibonacci_Yield_tool(n):
+    a, b = 0, 1
+    while n > 0:
+        yield b
+        a, b = b, a + b
+        n -= 1
+
+
+def Fibonacci(n):
+    """
+    斐波那契数列
+    :param n: 斐波那契数
+    :return: 斐波那契数列list
+    """
+    # return [f for i, f in enumerate(Fibonacci_Yield_tool(n))]
+    return list(Fibonacci_Yield_tool(n))

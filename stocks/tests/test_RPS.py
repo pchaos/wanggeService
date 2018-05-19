3b827f78d8008b7b9e123221793c3ff7e37caf6d
pchaosgit <drifthua@gmail.com>
2018-05-19 14:24:26 +0800

    init test_RPS.py

---
+++ stocks/tests/test_RPS.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+
+@File    : test_RPS.py
+
+Description :
+
+@Author :       pchaos
+
+date：          18-5-18
+-------------------------------------------------
+Change Activity:
+               18-5-18:
+@Contact : p19992003#gmail.com                   
+-------------------------------------------------
+"""
+from django.test import TestCase
+from stocks.models import Listing, STOCK_CATEGORY
+from stocks.models import RPSprepare, RPS
+import datetime
+
+__author__ = 'pchaos'
+
+
+class TestRPS(TestCase):
+    pass

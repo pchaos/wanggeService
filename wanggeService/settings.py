51be8fecb5d12510c45f602ecd70c2124c60051a
pchaosgit <drifthua@gmail.com>
2018-07-03 12:37:01 +0800

    增加productcategory.py

--- wanggeService/settings.py
+++ wanggeService/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'mptt',
     'stocks.apps.WanggesConfig',
     'comment.apps.CommentConfig',
     'polls.apps.PollsConfig',

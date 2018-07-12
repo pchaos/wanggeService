166389798d65e9f6efcf1b78a1baea9406050d61
pchaosgit <drifthua@gmail.com>
2018-07-12 18:01:01 +0800

    更新FrshHigh更新数据逻辑

--- stocks/models/freshhigh.py
+++ stocks/models/freshhigh.py
@@ -18,11 +18,12 @@ Change Activity:
 __author__ = 'pchaos'
 
 from django.db import models
-from django.db.models import Q, Max
+from django.db.models import Q, Max, Min
 from django.db import transaction
 import datetime
 import pandas as pd
 
+from stocks.models import convertToDate
 from stocks.models import StockBase
 from stocks.models import Listing, RPS
 import QUANTAXIS as qa
@@ -60,12 +61,15 @@ class FreshHigh(StockBase):
             :param m:
             :return:
             """
+
             def cmpfuncPeriod(df, days):
                 # 标记创半年新高
                 return pd.DataFrame(df.high == df.high.rolling(days).max())
 
             # 半年新高
             tdate = Listing.getNearestTradedate(days=-(n + m))
+            if start and (start - datetime.timedelta(m)) < tdate:
+                tdate = start - datetime.timedelta(m)
 
             data = qa.QA_fetch_stock_day_adv(code, start=tdate, end=end).to_qfq()
             ind = data.add_func(lambda x: cmpfuncPeriod(x, m))
@@ -84,12 +88,15 @@ class FreshHigh(StockBase):
         if not start:
             # 默认根据n，m周期计算获取数据开始日期
             start = cls.getNearestTradedate(days=-(n))
+        else:
+            start = convertToDate(start)
 
         # 查找大于start日期的RPS强度
         code = pd.DataFrame(list(RPS.getlist().filter(tradedate__gte=start, tradedate__lte=end).filter(
             (Q(rps120__gte=90) & Q(rps250__gte=80)) | (Q(rps120__gte=80) & Q(rps250__gte=90))). \
                                  values('code__code').distinct()))['code__code'].values.tolist()
-        df = firstHigh(code, start, end, n, m)
+        # df = firstHigh(code, start, end, n, m)
+        df = firstHigh(code[:20], start, end, n, m)
         with transaction.atomic():
             for v in [df.iloc[a] for a in df.index]:
                 print(v.code, v.date)
@@ -101,14 +108,42 @@ class FreshHigh(StockBase):
                     ddf['low'] = ddf['low'].apply(lambda x: round(x, 3))
                     ddf['date'] = ddf['date'].apply(lambda x: x.date())
                     c = Listing.getlist('stock').get(code=v['code'])
-                    qs = cls.objects.all().filter(code=c).aggregate(Max("ltradedate"))
+                    qs = cls.objects.all().filter(code=c).aggregate(Max("ltradedate"), Min("htradedate"))
                     if qs['ltradedate__max']:
-                        ddf = ddf[ddf['date'].apply(lambda x: x >= qs['ltradedate__max'])]
+                        # 数据库中有历史记录
+                        val = cls.getLastestOnCode(c)
+                        if val:
+                            #
+                            try:
+                                if (ddf[ddf['date'] == val['htradedate']]['high'] != float(val['high'])).iloc[0]:
+                                    # 最后一次高点与保存的数据不同，则表示有新的除权，删除历史数据，重新保存
+                                    cls.objects.all().filter(code=code).delete()
+                                else:
+                                    ddfold = ddf[ddf['date'].apply(lambda x: x < qs['htradedate__min'])]
+                                    if len(ddfold) > 0:
+                                        # 如果需要更令更老的数据，则删除重建
+                                        cls.objects.all().filter(code=code).delete()
+                                    else:
+                                        ddf = ddf[ddf['date'].apply(lambda x: x >= qs['ltradedate__max'] or x < qs['htradedate__min'])]
+                            except Exception as e:
+                                # ddf中没找到相关数据
+                                pass
+
                     entries = ddf.to_dict('records')
                     cls.updateHigh(c, entries)
 
                 except Exception as e:
                     print(e.args)
+
+    @classmethod
+    def getLastestOnCode(cls, code):
+        ld = cls.objects.all().filter(code=code).aggregate(Max("ltradedate"))['ltradedate__max']
+        qs = cls.objects.all().filter(code=code, ltradedate=ld)
+        if qs.count() > 0:
+            # 数据库记录有新的除权信息
+            return qs.values().first()
+        else:
+            return None
 
     @classmethod
     def updateHigh(cls, code, entries):
@@ -119,12 +154,12 @@ class FreshHigh(StockBase):
                 ld = cls.objects.all().filter(code=code).aggregate(Max("ltradedate"))['ltradedate__max']
                 qs = cls.objects.all().filter(code=code, ltradedate=ld)
                 if qs.count() > 0:
-                    # 有历史数据
+                    # 有历史数据,并且不是历史数据之前的数据
                     val = qs.values().first()
-                    l = val['low']
-                    h = val['high']
-                    hd = val['htradedate']
-                    # cls.update(code, val['high'], val['htradedate'],l, v['date'])
+                    if v['date'] >= val['htradedate']:
+                        l = float(val['low'])
+                        h = float(val['high'])
+                        hd = val['htradedate']
 
             if h < v['high']:
                 if h > 0 and l > v['low']:
@@ -142,13 +177,11 @@ class FreshHigh(StockBase):
 
     @classmethod
     def update(cls, code, high, htradedate, low, ltradedate):
-        try:
-            freshhigh = cls.objects.get(code=code, htradedate=htradedate)
-            freshhigh.low = low
-            freshhigh.ltradedate = ltradedate
-            freshhigh.save()
-        except Exception as e:
-            raise Exception(e)
+        freshhigh = cls.objects.get(code=code, htradedate=htradedate)
+        freshhigh.low = low
+        freshhigh.ltradedate = ltradedate
+        freshhigh.save()
+
 
     def __str__(self):
         return '{} {} {} {} {} '.format(self.code, self.high, self.htradedate, self.low, self.ltradedate)

b095e8f02e8da33c91dfa0ddec064246a9f2d8d6
pchaos <drifthua@gmail.com>
2018-12-12 11:57:44 +0800

    update 基金占比

--- stocks/models/jjcg.py
+++ stocks/models/jjcg.py
@@ -1,11 +1,17 @@
-
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+import numpy as np
+import time, datetime
 import QUANTAXIS as QA
-from pandas import pandas as pd , concat
+from pandas import pandas as pd, concat
 import datetime
+from stocks.models import convertToDate
 
 QA.QA_util_log_info('股票列表')
-data=QA.QAFetch.QATdx.QA_fetch_get_stock_list('stock')
-codelist=[ i[0] for i in data.index]
+data = QA.QAFetch.QATdx.QA_fetch_get_stock_list('stock')
+# 获取股票代码
+codelist = [i[0] for i in data.index]
 
 
 def get_jjp_report(code, report_date=[]):
@@ -34,7 +40,24 @@ def get_jjp_report(code, report_date=[]):
     return concat([df, df1], axis=1)
 
 
-today= '{}'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
+def get_report_date_list(endDate='{}'.format(datetime.datetime.now().strftime('%Y-%m-%d')), listCount=3):
+    ''' 返回endDate日期前的交易日期
+
+    :param endDate: 查询截止日期；默认为当前日期
+    :param listCount:  返回报告日期列表个数；listCount = 0 时，返回上一年到目前报告日期列表
+    :return: 返回值例子： ['2018-03-31', '2018-06-30', '2018-09-30']
+    '''
+    baselist = ['03-31', '6-30', '9-30', '12-31']
+    lastYear = convertToDate(endDate).year
+    firstYear = lastYear - listCount // 4 - 1
+    alist = []
+    for i in range(firstYear, lastYear):
+        for j in range(len(baselist)):
+            alist.append('{}-{}'.format(i, baselist[j]))
+    return alist[-listCount:]
+
+
+today = '{}'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
 df = get_jjp_report(codelist, ['2018-03-31', '2018-06-30', '2018-09-30'])
 # myres= res['fundsShareholding']/ res['totalCapital']
 
@@ -45,16 +68,15 @@ df[((df['percent'] > 0.03) | ((df['percent'] < 0.03) & (df['percent'] + df['sssp
 
 # 最后周期
 # todo 自动计算最后周期
-lastperoid='2018-09-30'
-preperoid='2018-06-30'
-jjp=df[((df['report_date']==lastperoid) & (df['percent']>0.03))]
+lastperoid = '2018-09-30'
+preperoid = '2018-06-30'
+jjp = df[((df['report_date'] == lastperoid) & (df['percent'] > 0.03))]
 # 最后周期数据未出,查询上个周期
-df1=df[(((df['report_date']==lastperoid) & (df['percent']==0.0)))]
-df2=df[((df['report_date']==preperoid) & (df['percent']>0.03))]
-st1=set(df1.code)
-st2=set(df2.code)
+df1 = df[(((df['report_date'] == lastperoid) & (df['percent'] == 0.0)))]
+df2 = df[((df['report_date'] == preperoid) & (df['percent'] > 0.03))]
+st1 = set(df1.code)
+st2 = set(df2.code)
 # 最后周期数据未出，上个周期基金持股大于等于3%的股票代码
-precodelist=list(st1 & st2)
+precodelist = list(st1 & st2)
 
-l=list(set(jjp.code) | set(precodelist))
-
+l = list(set(jjp.code) | set(precodelist))

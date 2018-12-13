3b2c1cd259cb42cd2653fa66d62d4fb2c5857543
pchaos <drifthua@gmail.com>
2018-12-13 18:43:18 +0800

    update 基金占比

--- stocks/tests/models/test_jjp.py
+++ stocks/tests/models/test_jjp.py
@@ -14,7 +14,7 @@
 from unittest import TestCase
 
 # from django.test import TestCase
-from stocks.tools.jjcg import JJP
+from stocks.tools.jjcg import JJP, getCodeList
 
 __author__ = 'yg'
 
@@ -23,8 +23,8 @@ class TestJjp(TestCase):
     def test_get_report_date_list(self):
         rd = JJP.get_report_date_list()
         self.assertTrue(len(rd) > 0, '报告日期返回为空！')
-        lc=6
-        for i in range(0,lc):
+        lc = 6
+        for i in range(0, lc):
             rd = JJP.get_report_date_list(listCount=i)
             if i == 0:
                 self.assertTrue(len(rd) > 3, '报告日期返回为空！report_date 个数: {}'.format(len(rd)))
@@ -32,16 +32,15 @@ class TestJjp(TestCase):
                 self.assertTrue(len(rd) == i, '报告日期返回为空！')
 
     def test_get_report_date_list_atSomeDay(self):
-        endDate= '2018-01-01'
-        trueEndDate='2017-12-31'
+        endDate = '2018-01-01'
+        trueEndDate = '2017-12-31'
         self._check_reportdate_atsomeDay(endDate, trueEndDate)
-        endDate= '2018-03-01'
-        trueEndDate='2017-12-31'
+        endDate = '2018-03-01'
+        trueEndDate = '2017-12-31'
         self._check_reportdate_atsomeDay(endDate, trueEndDate)
-        endDate= '2018-04-01'
-        trueEndDate='2018-03-31'
+        endDate = '2018-04-01'
+        trueEndDate = '2018-03-31'
         self._check_reportdate_atsomeDay(endDate, trueEndDate)
-
 
     def _check_reportdate_atsomeDay(self, endDate, trueEndDate):
         rd = JJP.get_report_date_list(endDate=endDate)
@@ -57,13 +56,39 @@ class TestJjp(TestCase):
             self.assertTrue(rd[-1] == trueEndDate, '{}报告日期返回应为：{}！'.format(endDate, trueEndDate))
 
     def test_get_jjp_report(self):
-        # todo error
         code = ['000001']
-        self._check_get_jjp_report(code)
+        df1 = self._check_get_jjp_report(code)
 
-        # code = ['000001','000002']
-        # self._check_get_jjp_report(code)
+        code = ['000001', '000002']
+        df2 = self._check_get_jjp_report(code)
+        self.assertTrue(len(df2) > len(df1), '报告周期内股票越多，数据越多：{} > {}'.format(len(df2), len(df1)))
 
-    def _check_get_jjp_report(self, code):
-        df = JJP.get_jjp_report(code, JJP.get_report_date_list())
-        self.assertTrue(len(df) > 0, '无')
+    def test_get_jjp_report_fullcode(self):
+        # 获取股票代码
+        codelist = getCodeList()
+        code = codelist[:10]
+        df1 = self._check_get_jjp_report(code)
+        print(df1[-10:])
+
+        code = codelist[:100]
+        df2 = self._check_get_jjp_report(code)
+        self.assertTrue(len(df2) > len(df1), '报告周期内股票越多，数据越多：{} > {}'.format(len(df2), len(df1)))
+
+    def _check_get_jjp_report(self, code, report_date=JJP.get_report_date_list()):
+        df = JJP.get_jjp_report(code, report_date)
+        self.assertTrue(len(df) > 0, '无报告')
+        print('股票代码个数：{}'.format(len(df)))
+        return df
+
+    def test_filterPercent(self):
+        # 获取股票代码
+        codelist = getCodeList()
+        code = codelist[:50]
+        reportDate = JJP.get_report_date_list()
+        df1 = self._check_get_jjp_report(code, reportDate)
+        df = JJP.filterPercent(df1, reportDate)
+        s= '结果应比原始数据少： 原始个数：{}, 过滤后数据：{}'.format(len(df1), len(df))
+        self.assertTrue(len(df) < len(df1), s)
+        print(s)
+        self.assertTrue(len(df) > 0, '未选出数据')
+        
--- stocks/tools/jjcg.py
+++ stocks/tools/jjcg.py
@@ -6,17 +6,22 @@ import time, datetime
 import QUANTAXIS as QA
 from pandas import pandas as pd, concat
 import datetime
+import os
 
-QA.QA_util_log_info('股票列表')
-data = QA.QAFetch.QATdx.QA_fetch_get_stock_list('stock')
-# 获取股票代码
-codelist = [i[0] for i in data.index]
+
+def getCodeList():
+    QA.QA_util_log_info('股票列表')
+    data = QA.QAFetch.QATdx.QA_fetch_get_stock_list('stock')
+    # 获取股票代码
+    return [i[0] for i in data.index]
+
+
 DATE_FORMAT = '%Y-%m-%d'
 
 
 def convertToDate(date, dateformat=DATE_FORMAT):
     """ 转换为日期类型
-
+path
     :param date: DATE_FORMAT = '%Y-%m-%d'
     例如： '2017-01-01'
 
@@ -34,8 +39,11 @@ class JJP():
     基金占流通股比更新例
     '''
 
-    def __init__(self, code):
-        self.code = code
+    def __init__(self, cvspath=''):
+        if len(cvspath) == 0:
+            self.cvspath = '/tmp/'
+        else:
+            self.cvspath = cvspath
 
     @staticmethod
     def get_jjp_report(code, report_date=[]):
@@ -51,10 +59,12 @@ class JJP():
         '''
         # res=QA.QA_fetch_financial_report(['000002','000026','000415','000417','600100'],['2017-12-31','2018-03-31','2018-06-30','2018-09-30'])
         res = QA.QA_fetch_financial_report(code, report_date)
+        # 基金占比
         myres = res['fundsShareholding'] / res['listedAShares']
         df = pd.DataFrame(myres)
         df.columns = ['percent']
         df = df.reset_index()
+        #
         myres = res['socialSecurityShareholding'] / res['listedAShares']
         df1 = pd.DataFrame(myres)
         df1.columns = ['ssspercent']
@@ -85,28 +95,64 @@ class JJP():
                 alist.append('{}-{}'.format(lastYear, baselist[j]))
         return alist[-listCount:]
 
+    @staticmethod
+    def filterPercent(df, report_date=[], percent=0.03):
+        '''
+        获取基金持股占比大于percent的代码列表
+        :param df:
+        :param report_date:
+        :param percent:
+        :return:
+        '''
+        alist = []
+        for rd in report_date[::-1]:
+            # 报告日期反序
+            if rd == report_date[-1]:
+                # 第一次判断
+                tdf = JJP._filterPercent(df, report_date, percent)
+            else:
+                pass
+            if len(tdf)> 0:
+                alist.append(tdf)
+        return JJP._df2code(alist)
 
-today = '{}'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
-jjp= JJP()
-df = jjp.get_jjp_report(codelist, jjp.get_report_date_list())
-# myres= res['fundsShareholding']/ res['totalCapital']
+    @staticmethod
+    def _df2code(alist):
+        '''
+        将代码去重
+        :param alist:
+        :return:
+        '''
+        aset = set()
+        for df in alist:
+            aset = aset | set(df.code)
+        return list(aset)
 
-# 基金占比小于3%，基金+社保持股占比大于3%
-df[(df['percent'] < 0.03) & (df['percent'] + df['ssspercent'] >= 0.03)]
-df[df['percent'] >= 0.03].append(df[(df['percent'] < 0.03) & (df['percent'] + df['ssspercent'] >= 0.03)])
-df[((df['percent'] > 0.03) | ((df['percent'] < 0.03) & (df['percent'] + df['ssspercent'] >= 0.03)))]
+    @staticmethod
+    def _filterPercent(df, report_date, percent):
+        # df = df[((df['percent'] > percent) | ((df['percent'] < percent) & (df['percent'] + df['ssspercent'] >= percent)))]
+        return df[df['percent'] + df['ssspercent'] >= percent]
 
-# 最后周期
-# todo 自动计算最后周期
-lastperoid = '2018-09-30'
-preperoid = '2018-06-30'
-jjp = df[((df['report_date'] == lastperoid) & (df['percent'] > 0.03))]
-# 最后周期数据未出,查询上个周期
-df1 = df[(((df['report_date'] == lastperoid) & (df['percent'] == 0.0)))]
-df2 = df[((df['report_date'] == preperoid) & (df['percent'] > 0.03))]
-st1 = set(df1.code)
-st2 = set(df2.code)
-# 最后周期数据未出，上个周期基金持股大于等于3%的股票代码
-precodelist = list(st1 & st2)
-
-l = list(set(jjp.code) | set(precodelist))
+# today = '{}'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
+# jjp= JJP()
+# df = jjp.get_jjp_report(codelist, jjp.get_report_date_list())
+# # myres= res['fundsShareholding']/ res['totalCapital']
+#
+# # 基金占比小于3%，基金+社保持股占比大于3%
+# df[(df['percent'] < 0.03) & (df['percent'] + df['ssspercent'] >= 0.03)]
+# df[df['percent'] >= 0.03].append(df[(df['percent'] < 0.03) & (df['percent'] + df['ssspercent'] >= 0.03)])
+# df[((df['percent'] > 0.03) | ((df['percent'] < 0.03) & (df['percent'] + df['ssspercent'] >= 0.03)))]
+#
+# # 最后周期
+# lastperoid = '2018-09-30'
+# preperoid = '2018-06-30'
+# jjp = df[((df['report_date'] == lastperoid) & (df['percent'] > 0.03))]
+# # 最后周期数据未出,查询上个周期
+# df1 = df[(((df['report_date'] == lastperoid) & (df['percent'] == 0.0)))]
+# df2 = df[((df['report_date'] == preperoid) & (df['percent'] > 0.03))]
+# st1 = set(df1.code)
+# st2 = set(df2.code)
+# # 最后周期数据未出，上个周期基金持股大于等于3%的股票代码
+# precodelist = list(st1 & st2)
+#
+# l = list(set(jjp.code) | set(precodelist))
